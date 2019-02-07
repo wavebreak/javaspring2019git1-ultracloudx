23e170ae0b6afe413b6923fb06e08c1035b713b3
ultracloudx <alexander.ta2007@gmail.com>
2019-02-07 12:00:05 -0600

    finished

--- src/TextMsgExpander.java
+++ src/TextMsgExpander.java
@@ -12,6 +12,23 @@ public class TextMsgExpander {
         System.out.println("");
 
         // Check for abbreviations
+        if (userLine.indexOf("BFF") != -1) {
+            userLine = userLine.replace("BFF", "best friend forever");
+        }
+        if (userLine.indexOf("IDK") != -1) {
+            userLine = userLine.replace("IDK", "I don't know");
+        }
+        if (userLine.indexOf("JK") != -1) {
+            userLine = userLine.replace("JK", "just kidding");
+        }
+        if (userLine.indexOf("TMI") != -1) {
+            userLine = userLine.replace("TMI", "too much information");
+        }
+        if (userLine.indexOf("TTYL") != -1) {
+            userLine = userLine.replace("TTYL", "talk to you later");
+        }
+
+        //can use input "You're my BFF but IDK if that was TMI or not.. JK!! I'll TTYL!"
        
 
         // Output expanded text
